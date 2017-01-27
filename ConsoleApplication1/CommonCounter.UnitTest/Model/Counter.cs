b2061a4322a073641af07bfee0544b3538892940
onuryasar34 <onuryasar34@gmail.com>
2017-01-27 00:40:09 +0000

    test azure

--- ConsoleApplication1/CommonCounter.UnitTest/Model/Counter.cs
+++ ConsoleApplication1/CommonCounter.UnitTest/Model/Counter.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1.Model
         public virtual string Period { get; set; }
         public virtual int InitialValue { get; set; }
         public virtual int IncrementValue { get; set; }
-        public virtual int MaxValue { get; set; }
+        public virtual int MaxValue { get; set; } 
         public virtual bool Recyle { get; set; }
     }
 }
